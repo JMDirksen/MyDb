81b8035bad0da9c6376d26922383c6a09ef9b426
JMDirksen <jeftadirksen@gmail.com>
2022-04-17 11:13:30 +0200

    page include

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
 require("init.php");
+
+$pages = ['home', 'manage'];
+$page = (isset($_GET['p']) && in_array($_GET['p'], $pages, true)) ? "./$_GET[p].php" : "./home.php";
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>MyDb</title>
+  <title>MyDb</title>
 </head>
 
 <body>
-    <p>Hello world!</p>
+  <?php include($page); ?>
 </body>
 
 </html>
