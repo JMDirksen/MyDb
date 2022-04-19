d026d0135db07cb3400f40e657367dd89d0b3341
JMDirksen <jeftadirksen@gmail.com>
2022-04-19 22:01:56 +0200

    use single quotes

--- functions.php
+++ functions.php
@@ -1,23 +1,23 @@
 <?php
 
 function redirect($url = null) {
-  if(!$url) $url = $_SERVER['REQUEST_URI'];
-  header('Location: '.$url);
+  if (!$url) $url = $_SERVER['REQUEST_URI'];
+  header('Location: ' . $url);
   die();
 }
 
 function dump($var) {
-  echo "<pre>" . print_r($var, true) . "</pre>";
+  echo '<pre>' . print_r($var, true) . '</pre>';
 }
 
 function loginRequired($type = null) {
-  if (!$user = isLoggedIn()) redirect("/?p=login");
-  if(isset($type) && $type != $user['type']) redirect("/?p=login");
+  if (!$user = isLoggedIn()) redirect('/?p=login');
+  if (isset($type) && $type != $user['type']) redirect('/?p=login');
 }
 
 function isLoggedIn() {
   if (isset($_SESSION['id']) && isset($_SESSION['type'])) {
-    return ["id" => $_SESSION['id'], "type" => $_SESSION['type']];
+    return ['id' => $_SESSION['id'], 'type' => $_SESSION['type']];
   }
   return false;
 }
@@ -25,7 +25,7 @@ function isLoggedIn() {
 function valid($value, $type = 1) {
   switch ($type) {
     case 1:
-      return preg_match("/^[a-z0-9_]+$/", $value) === 1;
+      return preg_match('/^[a-z0-9_]+$/', $value) === 1;
       break;
   }
 }
