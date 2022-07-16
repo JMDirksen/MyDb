9cb4cf8a9e2654684b10fd6d62e3a91ccf7c1d7c
JMDirksen <jeftadirksen@gmail.com>
2022-07-16 13:54:26 +0200

    remove DEBUG

--- src/config.php
+++ src/config.php
@@ -7,6 +7,3 @@ define('DB_NAME', getenv('DB_NAME') ? getenv('DB_NAME') : 'mydb');
 define('DB_USER', getenv('DB_USER') ? getenv('DB_USER') : 'mydb');
 define('DB_PASS', getenv('DB_PASS') ? getenv('DB_PASS') : 'mydb');
 define('DB_DSN', sprintf('%s:dbname=%s;host=%s', DB_TYPE, DB_NAME, DB_HOST));
-
-// Debug
-define('DEBUG', getenv('DEBUG') ? getenv('DEBUG') : false);
--- src/init.php
+++ src/init.php
@@ -17,11 +17,6 @@ spl_autoload_register(function ($class) {
 
 ini_set('allow_url_fopen', 0);
 
-if (DEBUG) {
-    ini_set('error_reporting', E_ALL);
-    ini_set('display_errors', 1);
-}
-
 try {
     $dbh = new PDO(DB_DSN, DB_USER, DB_PASS);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
